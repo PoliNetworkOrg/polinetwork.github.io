eb3715112c19eb97c50ae1e69f400fd45285f223
Federico Armellini <armellini.federico@gmail.com>
2019-07-31 22:53:07 +0200

    test 13

--- js/components/search2.js
+++ js/components/search2.js
@@ -165,7 +165,7 @@ function showSearchResults(data2){
 				
 					item_link = item.id_link;
 					
-					item.class = item.class.replace("&apos:", "'");
+					item.class = item.class.replace("&apos;", "'");
 					if (item.platform == "TG")
 					{
 						item_html = '<li>';
