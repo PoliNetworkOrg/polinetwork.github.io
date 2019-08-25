a17cdb004f3413715cf8fd7f21d9935d77ccac6b
Federico Armellini <armellini.federico@gmail.com>
2019-08-25 14:53:14 +0200

    test

--- search/js/components/search2.js
+++ search/js/components/search2.js
@@ -172,7 +172,7 @@ function showSearchResults(data2){
 					{
 						item_html = '<li>';
 						item_html += '<a href="https://t.me/joinchat/' + item_link + '">';
-						item_html += '<img style="width:18px;" src="../img/tg.svg" />';
+						item_html += '<img style="width:18px;" src="../../img/tg.svg" />';
 						if (item.office)
 						{
 							item_html += "&nbsp;[";
@@ -186,7 +186,7 @@ function showSearchResults(data2){
 						item_html += escapeHtml((item.class))
 
 						item_html += "&nbsp;";
-						item_html += '<img style="width:18px;" src="../img/'+bandiera+'.png" />';
+						item_html += '<img style="width:18px;" src="../../img/'+bandiera+'.png" />';
 
 						item_html += '</a>';
 						item_html += '</li>';
@@ -196,7 +196,7 @@ function showSearchResults(data2){
 					{
 						item_html = '<li>';
 						item_html += '<a href="https://www.facebook.com/groups/' + item_link + '">';
-						item_html += '<img style="width:18px;" src="../img/fb.svg" />';
+						item_html += '<img style="width:18px;" src="../../img/fb.svg" />';
 						if (item.office)
 						{
 							item_html += "&nbsp;[";
@@ -210,7 +210,7 @@ function showSearchResults(data2){
 						item_html += escapeHtml(htmlDecode(item.class))
 						
 						item_html += "&nbsp;";
-						item_html += '<img style="width:18px;" src="../img/'+bandiera+'.png" />';
+						item_html += '<img style="width:18px;" src="../../img/'+bandiera+'.png" />';
 						
 						item_html += '</a>';
 						item_html += '</li>';
@@ -220,7 +220,7 @@ function showSearchResults(data2){
 					{
 						item_html = '<li>';
 						item_html += '<a href="https://chat.whatsapp.com/' + item_link + '">';
-						item_html += '<img style="width:18px;" src="../img/wa.svg" />';
+						item_html += '<img style="width:18px;" src="../../img/wa.svg" />';
 						if (item.office)
 						{
 							item_html += "&nbsp;[";
@@ -234,7 +234,7 @@ function showSearchResults(data2){
 						item_html += escapeHtml(htmlDecode(item.class))
 						
 						item_html += "&nbsp;";
-						item_html += '<img style="width:18px;" src="../img/'+bandiera+'.png" />';
+						item_html += '<img style="width:18px;" src="../../img/'+bandiera+'.png" />';
 						
 						item_html += '</a>';
 						item_html += '</li>';
