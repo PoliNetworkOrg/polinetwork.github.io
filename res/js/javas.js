8520ca047a4c228149c414b6813fd55951348184
Eliaxie <elia.maggioni@gmail.com>
2021-04-08 00:06:49 +0200

    navbar

--- res/js/javas.js
+++ res/js/javas.js
@@ -8,20 +8,6 @@
 
 */
 
-  document.getElementById("parallasse").onscroll = function(){
-
-          if ((document.getElementById("parallasse").scrollTop > "236") && (document.getElementById("parallasse").scrollTop < "1612")) {
-
-              document.getElementById("navigation").style.top = "-35px";
-
-          } else if (document.getElementById("parallasse").scrollTop < "236"){
-              document.getElementById("navigation").style.top = "0px";
-
-          }
-          if(document.getElementById("parallasse").scrollTop > "1200"){
-            document.getElementById("navigation").style.top = "-70px";
-          }
-      }
 
 
   const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
@@ -40,15 +26,6 @@
         document.getElementById("darkmode2").innerHTML = "Enable_Dark_Mode"
     }
   }
-
-  $("elem1").scroll(function() {
-    var $height = $(window).scrollTop();
-    if($height > 50) {
-      alert("ciao");
-    } else {
-      alert("funziono");
-    }
-  });
 
     toggleSwitch.addEventListener('change', switchTheme, false);
 
