138dfcd8d93933daf5e01a6ba87bb9a76a0619b0
Eliaxie <elia.maggioni@gmail.com>
2021-04-11 12:31:13 +0200

    faster loading dark mode, fixed homepage for mobile

--- res/js/javas.js
+++ res/js/javas.js
@@ -11,23 +11,6 @@
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 const toggleSwitchb = document.querySelector('.theme-switchb input[type="checkbox"]');
 
-  window.onload = function startintTheme(){
-    if(window.matchMedia('{prefers-color-scheme: dark}')){
-      document.documentElement.setAttribute('data-theme', 'dark');
-      document.getElementById("darkmodeinputID").checked = true;
-      document.getElementById("darkmodeinputID2").checked = true;
-      document.getElementById("darkmode").innerHTML = "Disable_Dark_Mode"
-      document.getElementById("darkmode2").innerHTML = "Disable_Dark_Mode"
-
-    } else {
-      document.documentElement.setAttribute('data-theme', 'light');
-      document.getElementById("darkmode").innerHTML = "Enable_Dark_Mode"
-      document.getElementById("darkmode2").innerHTML = "Enable_Dark_Mode"
-    }
-  }
-
-
-
   function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
