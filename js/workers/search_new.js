c6bbc7cef03efb45be026ad2b6e97a6ab7f08376
armellini.federico@gmail.com <armellini.federico@gmail.com>
2021-07-06 12:16:01 +0200

    groups, v18

--- js/workers/search_new.js
+++ js/workers/search_new.js
@@ -49,30 +49,30 @@ loaded = false;
  *	See main doc for this package
  */
 onmessage = function(e) {
-	var data = e.data;
-	
-	// Loading data if first startup
-	while (this.loaded !== true){
-		this.loaded = this._reload();
-	}
+    var data = e.data;
 
-	switch (data.op_type){
-		case "search":
-			data.data = this.search(
-				data.data.query,
-				data.data.join,
-				data.data.limit
-			);
+    // Loading data if first startup
+    while (this.loaded !== true) {
+        this.loaded = this._reload();
+    }
 
-			break;
-		case "preload":
-			// Only for warming the script up
-			break;
-		default:
-			// Nothing to do
-	}
-	
-	postMessage(data);
+    switch (data.op_type) {
+        case "search":
+            data.data = this.search(
+                data.data.query,
+                data.data.join,
+                data.data.limit
+            );
+
+            break;
+        case "preload":
+            // Only for warming the script up
+            break;
+        default:
+            // Nothing to do
+    }
+
+    postMessage(data);
 }
 
 /**
@@ -84,87 +84,87 @@ onmessage = function(e) {
  *	@param 	join 	The type of join for query: AND (default AND)
  *	@param 	limit 	Max elements to return (default 5, -1 disable)
  */
-search = function(query, join, limit){
-	// Setting defaults
-	// if (join === undefined) {
-	// 	join = "AND";
-	// }
+search = function(query, join, limit) {
+    // Setting defaults
+    // if (join === undefined) {
+    // 	join = "AND";
+    // }
 
-	if (limit === undefined) {
-		limit = 5;
-	}
+    if (limit === undefined) {
+        limit = 5;
+    }
 
-	return this.index_data
-		.filter(item => this._filter(item, query))
-		.slice(0, (limit === -1?undefined:limit));
-	// // Filtering data
-	// var result = [];
+    return this.index_data
+        .filter(item => this._filter(item, query))
+        .slice(0, (limit === -1 ? undefined : limit));
+    // // Filtering data
+    // var result = [];
 
-	// Object.keys(query).forEach((item) => {
-	// 	result.push(
-	// 		this._filter(item, query)
-	// 	);
-	// });
+    // Object.keys(query).forEach((item) => {
+    // 	result.push(
+    // 		this._filter(item, query)
+    // 	);
+    // });
 
-	// // Joining Data
-	// if (join == "AND"){
-	// 	// AND
+    // // Joining Data
+    // if (join == "AND"){
+    // 	// AND
 
-	// 	this.index_data.filter(
-	// 		item => this._filter(item, query)
-	// 	)
+    // 	this.index_data.filter(
+    // 		item => this._filter(item, query)
+    // 	)
 
 
-	// 	// var tmp = {};
-	// 	// var oksum = 0;
+    // 	// var tmp = {};
+    // 	// var oksum = 0;
 
-	// 	// result.forEach((curarr, curarrid) => {
-	// 	// 	curarr.forEach((item) => {
-	// 	// 		if (!tmp[item]){
-	// 	// 			tmp[item] = 0;
-	// 	// 		}
-	// 	// 		tmp[item] += Math.pow(2, curarrid);
-	// 	// 	});
-	// 	// 	oksum += Math.pow(2, curarrid);
-	// 	// });
+    // 	// result.forEach((curarr, curarrid) => {
+    // 	// 	curarr.forEach((item) => {
+    // 	// 		if (!tmp[item]){
+    // 	// 			tmp[item] = 0;
+    // 	// 		}
+    // 	// 		tmp[item] += Math.pow(2, curarrid);
+    // 	// 	});
+    // 	// 	oksum += Math.pow(2, curarrid);
+    // 	// });
 
-	// 	// result = Object.keys(tmp).filter((item) => {
-	// 	// 	if (tmp[item] === oksum){
-	// 	// 		return true;
-	// 	// 	}
-	// 	// 	else {
-	// 	// 		return false;
-	// 	// 	}
-	// 	// });
-	// }
-	// // else {
-	// // 	// OR
-	// // 	result = result.reduce((list, item) => {
-	// // 		return list.concat(item);
-	// // 	},[]);
-	// // }
+    // 	// result = Object.keys(tmp).filter((item) => {
+    // 	// 	if (tmp[item] === oksum){
+    // 	// 		return true;
+    // 	// 	}
+    // 	// 	else {
+    // 	// 		return false;
+    // 	// 	}
+    // 	// });
+    // }
+    // // else {
+    // // 	// OR
+    // // 	result = result.reduce((list, item) => {
+    // // 		return list.concat(item);
+    // // 	},[]);
+    // // }
 
-	// // result = this._remove_duplicates(result);
-	// // result.sort();
+    // // result = this._remove_duplicates(result);
+    // // result.sort();
 
-	// // Limiting data
-	// if (Number(limit) !== -1){
-	// 	result = result.slice(0, limit);
-	// }
+    // // Limiting data
+    // if (Number(limit) !== -1){
+    // 	result = result.slice(0, limit);
+    // }
 
-	// // Retriving Data Info
-	// // result = result.map(id => this.info_data[id]);
+    // // Retriving Data Info
+    // // result = result.map(id => this.info_data[id]);
 
-	// // Returning Data
-	// return result;
+    // // Returning Data
+    // return result;
 
 }
 
 /**
  *  Dummy function used only for warming up the worker
  */
-preload = function(){
-	return;
+preload = function() {
+    return;
 }
 
 /**
@@ -175,32 +175,29 @@ preload = function(){
  *
  *  @return `true` if dataset has been reloaded successfully, `false` on errors.
  */
-_reload = function(){
-	var xhttp = new XMLHttpRequest();
+_reload = function() {
+    var xhttp = new XMLHttpRequest();
 
-	xhttp.open('GET', '/data/search/groups17.json', false);
-	xhttp.send(null);
+    xhttp.open('GET', '/data/search/groups18.json', false);
+    xhttp.send(null);
 
-	if (xhttp.status === 200) {
-		try {
-			var tmp = JSON.parse(xhttp.responseText);
-			if (
-				(Array.isArray(tmp.index_data) === true)
-			){
-				this.index_data = tmp.index_data;
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		catch (e){
-			return false;
-		}
-	}
-	else {
-		return false;
-	}
+    if (xhttp.status === 200) {
+        try {
+            var tmp = JSON.parse(xhttp.responseText);
+            if (
+                (Array.isArray(tmp.index_data) === true)
+            ) {
+                this.index_data = tmp.index_data;
+                return true;
+            } else {
+                return false;
+            }
+        } catch (e) {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
 /**
@@ -211,26 +208,26 @@ _reload = function(){
  *
  *  @return True if the item matches the query, false otherwise
  */
-_filter = function(obj, query){
-	// var toReturn;
+_filter = function(obj, query) {
+    // var toReturn;
 
-	return Object.keys(query)
-		.map(item => obj[item] === null || query[item] === null || obj[item].match(query[item]))
-		.reduce((acc, item) => acc && item, true);
-	// toReturn = this.index_data.filter((item) => {
-	// 	if (value === null || item[key] === null || item[key].match(value)){
-	// 		return true;
-	// 	}
-	// 	else {
-	// 		return false;
-	// 	}
-	// });
+    return Object.keys(query)
+        .map(item => obj[item] === null || query[item] === null || obj[item].match(query[item]))
+        .reduce((acc, item) => acc && item, true);
+    // toReturn = this.index_data.filter((item) => {
+    // 	if (value === null || item[key] === null || item[key].match(value)){
+    // 		return true;
+    // 	}
+    // 	else {
+    // 		return false;
+    // 	}
+    // });
 
-	// toReturn = toReturn.map(item => item.id);
-	// // toReturn = this._remove_duplicates(toReturn);
-	// // toReturn.sort();
+    // toReturn = toReturn.map(item => item.id);
+    // // toReturn = this._remove_duplicates(toReturn);
+    // // toReturn.sort();
 
-	// return toReturn;
+    // return toReturn;
 }
 
 // /**
