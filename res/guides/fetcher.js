2501c1fa81309f2e4925a5484adb7b7b0ccf28f5
Eliaxie <elia.maggioni@gmail.com>
2021-07-18 16:39:00 +0200

    guides

--- res/guides/fetcher.js
+++ res/guides/fetcher.js
@@ -21,7 +21,8 @@ fetch(window.location.pathname.split('/').pop().split('.')[0] + '.txt')
     lines = data.split('\n')
     lines = lines.map(line => line.replace('\r', ''))
     l = lines.length
-    doc = '<body><div class="wrapper"><h2><span class="accent">./</span>Polinetwork_Guides<span class="accent">/</span>AES<span class="accent">/</span></h2><div class="title"><h1>'
+    path = window.location.pathname.split('/');
+    doc = '<body><div class="wrapper"><h2>.<span class="accent">/</span><a class="" href="/">Polinetwork</a><span class="accent">/</span><a class="" href="../">Guides</a><span class="accent">/</span><a class="" href="./">' + capitalizeFirstLetter(path[3]) + '</a><span class="accent">/</span></h2><div class="title"><h1>'
     document.title = lines[0]
     doc += lines[0]
     doc += '</h1></div>'
@@ -37,18 +38,12 @@ fetch(window.location.pathname.split('/').pop().split('.')[0] + '.txt')
     while (i < l) {
         // if a new course is beginning
         if (lines[i++].slice(0, 3) === '###') {
-            doc += '<div class="course"><h3>' + lines[i-1].slice(3) + '</h3>'
-            while (i < l && (lines[i].slice(0, 8) === 'TELEGRAM' || lines[i].slice(0, 8) === 'WHATSAPP' || lines[i].slice(0, 8) === 'MANIFEST'))
-                if (lines[i].slice(0, 8) === 'TELEGRAM'){
-                    doc += '<a href="' + lines[i].slice(8) + '"><img class="telegram" src="res/telegram.webp"></a>'
-                    i++;
-                } else if (lines[i].slice(0, 8) === 'WHATSAPP'){
-                    doc += '<a href="' + lines[i].slice(8) + '"><img class="telegram" src="res/wa.svg"></a>'
-                    i++;
-                }else{
-                    doc += '<a href="' + lines[i].slice(8) + '"><img class="manifest" src="res/manifest.png"></a>'
-                    i++
-                  }
+            doc += '<div class="course"><div class="collapsible"><h3>' + lines[i-1].slice(3) + '</h3>' + '<div class="containerFA"><i class="fa fa-angle-down"></i></div></div>' + '<div class="content">'
+            while (i < l && (lines[i].slice(0, 8) === 'TELEGRAM' || lines[i].slice(0, 8) === 'MANIFEST'))
+                if (lines[i++].slice(0, 8) === 'TELEGRAM')
+                    doc += '<a href="' + lines[i-1].slice(8) + '"><img class="telegram" src="/res/guides/telegram.webp"></a>'
+                else
+                    doc += '<a href="' + lines[i-1].slice(8) + '"><img class="manifest" src="/res/guides/manifest.png"></a>'
             for (;i < l; ++i) {
                 if (lines[i] === '') continue
                 // if a new course line is found exit from current course construction
@@ -64,9 +59,32 @@ fetch(window.location.pathname.split('/').pop().split('.')[0] + '.txt')
                 } // end of comment
             } // end of commentS
             doc += '</div>'
+            doc += '</div>'
         } // end of course
     } // end of courseS
     doc += '<div class="footer">Un\'iniziativa Polinetwork. Trova altro su <span class="accent"><a href="https://polinetwork.github.io">polinetwork.github.io</a></span>'
-    doc += '<br>Per commenti o suggerimenti di natura tecnica contatta @EliaMaggioni o @LuigiFusco su Telegram, per suggerimenti sui contenuti o per aggiungere un commento contatta @Raif9</div>'
+    doc += '<br>Per commenti o suggerimenti contatta @EliaMaggioni o @LuigiFusco su Telegram</div>'
     document.body.innerHTML = doc
-}).catch(e => alert('Error parsing the file, please contact an admin on Telegram (@EliaMaggioni) or write an email at polinetwork@outlook.com ! ' + e))
+
+    // buttons to expand sections
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+    for (i = 0; i < coll.length; i++) {
+      coll[i].addEventListener("click", function() {
+        var childNodes = this.childNodes;
+        for (var i = 0; i < childNodes.length; i++) {
+          childNodes[i].classList.toggle("active");  // <a>
+        }
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+}).catch(e => alert('Error parsing the file, please contact an admin! ' + e))
+
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
