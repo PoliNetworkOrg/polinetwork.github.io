932acc0755ad3d0be567e74cd34b30cf561004b5
Eliaxie <elia.maggioni@gmail.com>
2021-07-22 19:29:23 +0200

    dark mode

--- res/js/dark_mode/javas_en.js
+++ res/js/dark_mode/javas_en.js
@@ -14,13 +14,13 @@ const toggleSwitchb = document.querySelector('.theme-switchb input[type="checkbo
   function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        document.getElementById("darkmode").innerHTML = "Disable Dark Mode"
-        document.getElementById("darkmode2").innerHTML = "Disable Dark Mode"
+        document.getElementById("darkmode").innerHTML = "Disable&nbspDark&nbspMode"
+        document.getElementById("darkmode2").innerHTML = "Disable&nbspDark&nbspMode"
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
-        document.getElementById("darkmode").innerHTML = "Enable Dark Mode"
-        document.getElementById("darkmode2").innerHTML = "Enable Dark Mode"
+        document.getElementById("darkmode").innerHTML = "Enable&nbspDark&nbspMode"
+        document.getElementById("darkmode2").innerHTML = "Enable&nbspDark&nbspMode"
     }
   }
 
--- res/js/dark_mode/javas_it.js
+++ res/js/dark_mode/javas_it.js
@@ -14,13 +14,13 @@ const toggleSwitchb = document.querySelector('.theme-switchb input[type="checkbo
 function switchTheme(e) {
     if (e.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
-        document.getElementById("darkmode").innerHTML = "Disattiva Tema Scuro"
-        document.getElementById("darkmode2").innerHTML = "Disattiva Tema Scuro"
+        document.getElementById("darkmode").innerHTML = "Disattiva&nbspTema&nbspScuro"
+        document.getElementById("darkmode2").innerHTML = "Disattiva&nbspTema&nbspScuro"
     }
     else {
         document.documentElement.setAttribute('data-theme', 'light');
-        document.getElementById("darkmode").innerHTML = "Attiva Tema Scuro"
-        document.getElementById("darkmode2").innerHTML = "Attiva Tema Scuro"
+        document.getElementById("darkmode").innerHTML = "Attiva&nbspTema&nbspScuro"
+        document.getElementById("darkmode2").innerHTML = "Attiva&nbspTema&nbspScuro"
     }
 }
 
