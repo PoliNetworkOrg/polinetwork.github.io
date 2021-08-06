2348442958cdbcc9e840c2d26d75633e33b9eb1d
Federico Armellini <armellini.federico@gmail.com>
2021-08-06 13:51:27 +0200

    "polinetwork" => "PoliNetwork"

--- res/guides/fetcher.js
+++ res/guides/fetcher.js
@@ -27,9 +27,9 @@ fetch(filelocation.split('/').pop().split('.')[0] + '.txt')
     l = lines.length
     path = window.location.pathname.split('/');
     if(path.length > 4)
-      doc = '<body><div class="wrapper"><h2>.<span class="accent">/</span><a class="" href="/">Polinetwork</a><span class="accent">/</span><a class="" href="../">' + capitalizeFirstLetter(path[2]) + '</a><span class="accent">/</span><a class="" href="./">' + capitalizeFirstLetter(path[3]) + '</a><span class="accent">/</span></h2><div class="title"><h1>'
+      doc = '<body><div class="wrapper"><h2>.<span class="accent">/</span><a class="" href="/">PoliNetwork</a><span class="accent">/</span><a class="" href="../">' + capitalizeFirstLetter(path[2]) + '</a><span class="accent">/</span><a class="" href="./">' + capitalizeFirstLetter(path[3]) + '</a><span class="accent">/</span></h2><div class="title"><h1>'
     if(path.length > 3)
-      doc = '<body><div class="wrapper"><h2>.<span class="accent">/</span><a class="" href="/">Polinetwork</a><span class="accent">/</span><a class="" href="../">' + capitalizeFirstLetter(path[2]) + '</a><span class="accent">/</span></h2><div class="title"><h1>'
+      doc = '<body><div class="wrapper"><h2>.<span class="accent">/</span><a class="" href="/">PoliNetwork</a><span class="accent">/</span><a class="" href="../">' + capitalizeFirstLetter(path[2]) + '</a><span class="accent">/</span></h2><div class="title"><h1>'
     document.title = lines[0]
     doc += lines[0]
     doc += '</h1></div>'
@@ -71,11 +71,11 @@ fetch(filelocation.split('/').pop().split('.')[0] + '.txt')
     } // end of courseS
     let oppositeLangPath = window.location.pathname.split('/').slice(2,window.location.pathname.length).join("/");
     if (window.location.pathname.split('/')[1] == "it"){
-      doc += '<div class="footer">Un\'iniziativa Polinetwork. Trova altro su <span class="accent"><a href="https://polinetwork.github.io">polinetwork.github.io</a></span>'
+      doc += '<div class="footer">Un\'iniziativa PoliNetwork. Trova altro su <span class="accent"><a href="https://polinetwork.github.io">polinetwork.github.io</a></span>'
       doc += '<br>Per commenti o suggerimenti contatta <a href="https://t.me/eliamaggioni" class="accent">@EliaMaggioni</a> o <a href="https://t.me/LuigiFusco" class="accent">@LuigiFusco</a> su Telegram</div>'
       doc += '<div"><span class="accent"><a href="/en/' + oppositeLangPath + '">Switch to EN</a></span></div>'
     } else {
-      doc += '<div class="footer">A Polinetwork initiative. Find more at <span class="accent"><a href="https://polinetwork.github.io">polinetwork.github.io</a></span>'
+      doc += '<div class="footer">A PoliNetwork initiative. Find more at <span class="accent"><a href="https://polinetwork.github.io">polinetwork.github.io</a></span>'
       doc += '<br>For comments or suggestions contact <a href="https://t.me/eliamaggioni" class="accent">@EliaMaggioni</a> or <a href="https://t.me/LuigiFusco" class="accent">@LuigiFusco</a> on Telegram</div>'
       doc += '<div"><span class="accent"><a href="/it/' + oppositeLangPath + '">Switch to IT</a></span></div>'
     }
