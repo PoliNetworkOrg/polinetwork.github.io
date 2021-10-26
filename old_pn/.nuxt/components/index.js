67dc3574679824d434396a93496ff8423e7d98ab
Federico Armellini <armellini.federico@gmail.com>
2021-10-26 11:00:17 +0200

    moved old website to https://github.com/PoliNetworkOrg/OldWebsite

--- old_pn/.nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-import { wrapFunctional } from './utils'
-
-export { default as Logo } from '../..\\components\\Logo.vue'
-
-export const LazyLogo = import('../..\\components\\Logo.vue' /* webpackChunkName: "components/logo" */).then(c => wrapFunctional(c.default || c))
